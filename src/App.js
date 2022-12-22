66b2af815fd2ee4dd41fb01f0a11c8a1d1b4134a
Ezekwu Jerry <ezekwujerry@gmail.com>
2022-12-22 17:17:58 +0100

    completd hero section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Homepage from './pages/Homepage';
+import Project from './pages/Project';
 
 
 function App() {
   return (
     <div className="App">
-      hello world
+      <Router>
+        <Routes>
+          <Route path='/' element={<Homepage/>}/>
+          <Route path='/project' element={<Project/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }

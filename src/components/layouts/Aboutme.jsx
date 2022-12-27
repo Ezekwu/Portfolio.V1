33626f0c81ea3b039c139380b830f054da64ce51
Ezekwu Jerry <ezekwujerry@gmail.com>
2022-12-27 16:09:07 +0100

    added extra files

---
+++ src/components/layouts/Aboutme.jsx
@@ -0,0 +1,71 @@
+import { StyledAmoutme } from "../styles/Aboutme.styled"
+import CodeSvg from '../../assets/svg/code-svgrepo-com.svg'
+import StarSvg from '../../assets/svg/star-favourite-rating-svgrepo-com.svg'
+import { motion } from "framer-motion"
+import { variants } from "../animations/variants"
+const Aboutme = () => {
+    const aboutTitle = "About me"
+    const titleArr = aboutTitle.split(' ')
+    const {transitionX, transitionYFunc, fadeInFunc  }  = variants
+    return (
+        
+        <StyledAmoutme>
+            
+            <div className="container wrapper">
+                <div className="col-1">
+                    <motion.div 
+                    className="about-text_wrapper"
+                    variants={transitionX(0.04).parent}
+                    whileInView="animate"
+                    initial="initial">
+                        {
+                            titleArr.map((word, index)=>(
+                                <div className="span-parent" key={index}>
+                                    <motion.div 
+                                    className="span-child"
+                                    variants={transitionX().child}>
+                                        {word}
+                                    </motion.div>
+                                </div>
+                            ))
+                        }
+                    </motion.div>
+                </div>
+
+                <motion.div 
+                className="col-2"
+                variants={fadeInFunc(1).parent}
+                whileInView="animate"
+                initial="initial">
+                    <motion.div 
+                    className="point"
+                    variants={fadeInFunc().child}>
+                        <img src={CodeSvg} alt="" />
+                        <p>I am a meticulous front-end developer with 2 years of experience building responsive and interactive websites</p>
+                    </motion.div>
+                    <motion.div 
+                    className="point"
+                    variants={fadeInFunc().child}>
+                        <img src={CodeSvg} alt="" />
+                        <p>I am a good problem solver, often blending my creativity with my passion to provide efficient solutions for my clients</p>
+                    </motion.div>
+                    <motion.div 
+                    className="point"
+                    variants={fadeInFunc().child}>
+                        <img src={CodeSvg} alt="" />
+                        <p>I love being part of a team that includes individuals that are passionate about their jobs, and execute their tasks to perfection</p>
+                    </motion.div>
+                    <motion.div 
+                    className="point"
+                    variants={fadeInFunc().child}>
+                        <img src={StarSvg} alt="" className="star-svg"/>
+                        <p>Asides web-development, i love cooking, working out and listening to  some good music</p>
+                    </motion.div>
+
+                </motion.div>
+            </div>
+        </StyledAmoutme>
+    )
+}
+
+export default Aboutme
---
+++ src/components/styles/Aboutme.styled.js
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+export const StyledAmoutme = styled.section`
+background-color: var(--primaryBlue);
+color: var(--geryBackground);
+
+font-size: 4rem;
+padding: 4rem 0;
+&>div{
+    display: flex;
+    gap: 10rem;
+    .col-1{
+        
+        .about-text_wrapper{
+            display: flex;
+            gap: .5rem;
+            font-size: 2.3rem;
+            display: flex;
+            overflow: hidden;
+            .span-parent{
+                overflow: hidden;
+                display: flex;
+                .span-child{
+                    font-family: 'Noto Nastaliq Urdu', serif;
+
+                }
+            }
+        }
+    }
+
+    .col-2{
+        font-size: 1.2rem;
+        display: grid;
+        column-gap: 2rem;
+        row-gap: 3rem;
+        grid-template-columns: repeat(2, 1fr);
+        grid-template-columns: repeat(2, 1fr);
+
+        .point{
+            display: flex;
+            align-items: flex-start;
+            gap: 1rem;
+            img{
+                width: 22px;
+            }
+            .star-svg{
+                width: 33px;
+            }
+        }
+    }
+}
+`
